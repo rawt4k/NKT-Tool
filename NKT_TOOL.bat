318ead7a436480973b30d32582b17f27a1f42258
Ven <91895035+Ventispurr@users.noreply.github.com>
2022-11-13 16:09:29 -0500

    Update NKT_TOOL.bat

--- NKT_TOOL.bat
+++ NKT_TOOL.bat
@@ -1,6 +1,6 @@
 @echo off
-title Nico Knows Tech's Windows Fix Script v1.0
-echo Nico Knows Tech's Windows Fix Script v1.0
+title Nico Knows Tech's Windows Fix Script v1.1
+echo Nico Knows Tech's Windows Fix Script v1.1
 echo https://youtube.com/NicoKnowsTech
 echo.
 echo NOTICE: THIS SCRIPT ONLY RUNS COMMANDS BUILT INTO WINDOWS. NO THIRD PARTY APPLICATIONS OR TOOLS
@@ -30,6 +30,25 @@ echo.
 echo --- DISM REPAIR STARTED ---
 DISM /Online /Cleanup-Image /RestoreHealth
 echo Done...
+echo.
+echo --- Temp File Removal Started ---
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+echo Done!
 echo.
 echo All tasks have completed successfully!
 echo You can press any key to close this window. Reboot your PC.
